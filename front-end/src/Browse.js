0029c916bc0b902a1d588495a50abd8fd00d1550
Ryan Tate <rtate@willistonfinancial.com>
2018-08-19 02:23:47 -0700

    use dropdowns for browse on mobile

--- front-end/src/Browse.js
+++ front-end/src/Browse.js
@@ -5,24 +5,50 @@ import './Browse.css';
 export default function Browse(props){
     return <div id={'browse'} className={props.is_mobile ? 'mobile' : null}>
         <nav id="browse-select" style={{width: props.is_mobile ? '100%' : props.window.height / (props.letters.length || 26)}}>
-            {props.letters.map(l => <button
-                key={l}
-                className={l === props.browse_letter ? 'active' : ''}
-                onClick={() => props.dispatch({type: 'browse_letter', letter: l})}
-            >
-                {l.toUpperCase()}
-            </button>)}
+            {
+                props.is_mobile ?
+                    <label>
+                        Letter
+                        <select
+                            value={props.browse_letter}
+                            onChange={e => props.dispatch({type: 'browse_letter', letter: e.target.value})}>
+                            {
+                                props.letters.map(l => <option key={l} value={l.toLowerCase()}>{l.toUpperCase()}</option>)
+                            }
+                        </select>
+                    </label>
+                    :
+                    props.letters.map(l => <button
+                        key={l}
+                        className={l === props.browse_letter ? 'active' : ''}
+                        onClick={() => props.dispatch({type: 'browse_letter', letter: l})}
+                    >
+                        {l.toUpperCase()}
+                    </button>)
+            }
         </nav>
         <div id="browse-words">
             {
-                props.browse_words.map(word => <div
-                    key={word}
-                    id={word.toLowerCase().replace(/\s/g, '_')}
-                    className={`browse-word ${word === props.browse_word ? 'active' : ''}`}
-                    onClick={() => props.dispatch({type: 'browse_word', word})}
-                >
-                    {word}
-                </div>)
+                props.is_mobile ?
+                    <label>
+                        Word
+                        <select
+                            value={props.browse_word}
+                            onChange={e => props.dispatch({type: 'browse_word', word: e.target.value})}>
+                            {
+                                props.browse_words.map(word => <option key={word}>{word}</option>)
+                            }
+                        </select>
+                    </label>
+                    :
+                    props.browse_words.map(word => <div
+                        key={word}
+                        id={word.toLowerCase().replace(/\s/g, '_')}
+                        className={`browse-word ${word === props.browse_word ? 'active' : ''}`}
+                        onClick={() => props.dispatch({type: 'browse_word', word})}
+                    >
+                        {word}
+                    </div>)
             }
         </div>
         {
